2ecf8afc21e3f408822dfd86e3d20cdc228b3ea4
KrisztianB9 <bacsokrisztian09@gmail.com>
2025-11-04 18:43:50 +0200

    new background

--- src/main/java/ro/umfst/oop/musicapp/MainApp.java
+++ src/main/java/ro/umfst/oop/musicapp/MainApp.java
@@ -4,12 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainApp extends Application {
